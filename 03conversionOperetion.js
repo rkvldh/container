b8367120cd05589a1e7b3ba1d44617a05e684ce3
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2023-09-01 04:16:53 +0000

    code start

--- 03conversionOperetion.js
+++ 03conversionOperetion.js
@@ -1,7 +1,9 @@
 // stack memory => Primitive Types , Heep Memory => Non Primitive types
 let myName = 'abcdefghijklmynpqurxt'
-let anotherName = 'fghijklmynp';
-// time 02:34 minut
+let anotherName = myName;
+// time02:00 hour
+
+
 
 
 
