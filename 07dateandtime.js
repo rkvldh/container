c5d542d58d5cdea2fcef46941f30b9254cd844e4
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2023-09-02 04:19:11 +0000

    sum code write  light gone

---
+++ 07dateandtime.js
@@ -0,0 +1,7 @@
+// date and time
+
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
