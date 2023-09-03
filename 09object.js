cd089437ff80e23f67f126fb8168324162883acc
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2023-09-03 03:41:59 +0000

    save changes

---
+++ 09object.js
@@ -0,0 +1,33 @@
+// singleton using constructer
+
+// object litrsl  ,new empty object
+
+const mySym = Symbol("key1") ;
+
+const jsuser = {
+    [mySym]: "mykey1",
+    'full name': 'Ramesh Kumar',
+    name: 'Ramesh',
+    age: 44,
+    weight: 62,
+    locetion: 'punjab',
+    email: 'abc@email.com',
+    lastLoginDays: ['monday','suterday']
+
+}; 
+
+// jsuser.email = "ramesh@chat.com";
+// Object.freeze(jsuser);
+// jsuser.email = "kumar@gpt.com";
+
+// console.log(jsuser.email);
+// console.log(jsuser["full name"]);
+// console.log(jsuser[mySym]);
+
+jsuser.greeting = function(){
+    console.log('hello js user');
+}
+console.log(jsuser.greeting())
+
+
+
