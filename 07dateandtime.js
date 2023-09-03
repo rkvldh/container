da3f7829109001a808d1db247228e9142569604c
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2023-09-03 03:14:37 +0000

    array practice

--- 07dateandtime.js
+++ 07dateandtime.js
@@ -1,30 +1,37 @@
-// date and time
+// date object
 
-// let myDate = new Date(); // date method new Date().toString();
+let myDate = new Date();
 // console.log(myDate.toString());
-// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
 // console.log(myDate.toLocaleDateString());
-// console.log(typeof myDate);
+// console.log(myDate.toLocaleString(), "to local string");
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
 
-// let createdDate = new Date(2023, 8,23);
-// let createdDate = new Date(2023, 8,23,5,3);
-// let createdDate = new Date("2023-03-14");
-// let createdDate = new Date("01-14-2023");
-// console.log(createdDate.toDateString());
+// let myCretedDate = new Date(2023, 9, 3);
+// let myCretedDate = new Date(2020, 9, 3, 5, 3);
+// let myCretedDate = new Date("2020-9-3");
+// let myCretedDate = new Date("9-23-2020");
+// console.log(myCretedDate.toDateString());
 
-// let myTimeStamp =  Date.now()
+// let myTimeStamp = new Date();
 // console.log(myTimeStamp);
-// console.log(myTimeStamp.getTime());  // a pta nhi lga error kyon aaya
-// console.log(Math.floor(Date.now()));
+
+// console.log(myTimeStamp.getTime());
+// console.log(Math.round(Date.now() / 100));
 
 let newDate = new Date();
-console.log(newDate.getFullYear()); // get year
-console.log(newDate.getMonth() + 1);
+console.log(newDate.getFullYear(), "full year");
+console.log(newDate.getDate());
 console.log(newDate.getDay());
 console.log(newDate.getHours());
 console.log(newDate.getMinutes());
-
-let nmonth = newDate.toLocaleDateString('default',{
-    weekday: "long"
-})
-// 03:10 minut
+console.log(newDate.getMonth() + 1);
+// console.log(newDate.getMilliseconds());
+console.log(
+  newDate.toLocaleString("default", {
+    weekday: "long",
+  })
+);
+// time is 03:10 minut
---
+++ 08basicArray.js
@@ -0,0 +1,58 @@
+// Arrays
+
+// const myArray = [0, 1, 2, 3, 4, 5];
+// const myHeros = ["shaktiman", "nagraj"];
+// console.log(myHeros.length);
+// console.log(myArray);
+// console.log(myArray[2]);
+// //  Array Method
+// myArray.push(6);
+// console.log(myArray);
+// myArray.pop();
+// console.log(myArray);
+// myArray.unshift(9);
+// myArray.shift();
+// console.log(myArray);
+// console.log(myArray.includes(9));
+// console.log(myArray.indexOf(3));
+
+// const newArr = myArray.join();
+// console.log(newArr);
+// slice ,splice
+// console.log("A", myArray);
+// const myn1 = myArray.slice(1, 3);
+// console.log(myn1);
+// console.log("B slice method not remove is orignal array", myArray);
+// const myn2 = myArray.splice(1, 3);
+// console.log("C with splice method remove is orignal array ", myArray);
+// console.log('',myn2);
+// // time 03:30 minut
+
+// array inside array 
+const marvel_heros = ["thor","Ironman","spiderman"];
+const dc_heros = ["superman","flash","batman"];
+// marvel_heros.push(dc_heros);
+// console.log(marvel_heros); 
+// console.log(marvel_heros[3][1]);
+// concat murge multi array in one array
+
+//  const newHeros = marvel_heros.concat(dc_heros);
+//  console.log(newHeros);
+
+// const newSpreadHeros = ["first heros",...marvel_heros,...dc_heros,"Last heros"]
+// console.log(newSpreadHeros);
+
+const anotherArray = [1,2,3,[4,5,6],[7,8,9,[10,11,12]]];
+console.log(anotherArray);
+const real_another_array = anotherArray.flat(Infinity);
+console.log(real_another_array,'flat 1 array inside multi array is one array using flat method');
+ 
+console.log('kya array hai',Array.isArray('ramesh'));
+console.log('convert string to array',Array.from('rameah'));
+
+console.log('not solve array return empty array',Array.from({name: 'ramesh'}));
+
+
+
+
+
