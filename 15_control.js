bdc40b6553d07a57cd087df1d9a2e10cec3c6c50
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2023-09-07 07:04:29 +0000

    read control in js

---
+++ 15_control.js
@@ -0,0 +1,95 @@
+// skip 05 :55 se 06:21 minut tak nhi pada basic copmlete
+// if statment
+// <, >, <=, >= ==, ===
+// const iuserloggedzin = true;
+// const temprature = 41;
+// if (temprature === 50) {
+//   console.log("tempraure less then 50");
+// }
+// console.log("temprature is gretear then 50");
+// ***************************************
+// if (iuserloggedzin) {
+//   console.log("User Login");
+// } else {
+//   console.log("User Not Login");
+// }
+// // *************************************
+// const score = 200;
+
+// if (score > 100) {
+//   let power = "fly";
+//   console.log(`user power: ${power}`);
+// }
+// // console.log(`user power: ${power}`);
+// *******************************************
+// shorthand notation
+// const balance = 1000;
+// es terah nhi likhna hai
+// if (balance > 500) console.log("test");
+// if (balance < 500) {
+//   console.log("less then 500");
+// } else if (balance < 750) {
+//   console.log("less then 750");
+// } else if (balance < 900) {
+//   console.log("less then 900");
+// } else {
+//   console.log("greater then 1200");
+// }
+// *********************************************
+// real life example
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedFromUser = false;
+// const loggedInFromEmail = true;
+
+// if (userLoggedIn && debitCard && 2 == 2) {
+//   console.log("Allow to bye course");
+// }
+// if (loggedFromUser || loggedInFromEmail) {
+//   console.log("User Logged In");
+// }
+// ***********************************************
+// switch statment
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 1;
+
+// switch (month) {
+//   case 1:
+//     console.log("Januery");
+//     break;
+//   case 2:
+//     console.log("Febuery");
+//     break;
+//   case 3:
+//     console.log("March");
+//     break;
+
+//   default:
+//     console.log("enter right value");
+//     break;
+// }
+// **************************************
+// Truthy  value
+// true, "0", 'false', ' ', [], {}, function(){}
+// ********************************************
+// const userEmail = [];
+// if (userEmail.length === 0) {
+//   console.log("array is empty");
+// }
+// *******************************************
+// if (userEmail) {
+//   console.log("Got user email");
+// } else {
+//   console.log("Don't have user email");
+// }
+
+// Falsy value false, 0, -0,Bigint 0n, "", null, undifined,nan
+// 07:00 minut
