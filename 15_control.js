29e2d6aa0f6e57e82af5d3810d225fd721123932
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2023-09-08 11:53:45 +0000

    resume coading

--- 15_control.js
+++ 15_control.js
@@ -93,3 +93,31 @@
 
 // Falsy value false, 0, -0,Bigint 0n, "", null, undifined,nan
 // 07:00 minut
+
+// Falsy value false, 0, -0,Bigint 0n, "", null, undifined,nan
+// 07:00 minut
+// const userEmail = [];
+// if (userEmail.length === 0) {
+//   console.log("array is empty");
+// }
+
+// const emptyObject = {};
+// if (Object.keys(emptyObject).length === 0) {
+//   console.log("object id empty");
+// }
+// ****and or operetor******
+// Nullish Coalescing operator (??) null undifined
+// let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15;
+// val1 = null ?? 10 ?? 20;
+// console.log(val1);
+// ******Ternery operator*******
+// condition ? true : false
+const iceTeaPrice = 100;
+iceTeaPrice >= 80
+  ? console.log("less then 80")
+  : (cl = console.log("more then 80"));
+// 07: 10 minut
+
