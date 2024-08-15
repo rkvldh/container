6703dd709a415bd5d73b3f51c6a936a36d6f0b31
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2024-08-15 15:00:23 +0000

    rest parameeter

---
+++ restParameeter.js
@@ -0,0 +1,17 @@
+// param dstructring
+// Object
+// react
+
+const person = {
+    firstName: 'ramesh',
+    gender:'male',
+}
+
+function printDetails(obj){
+    console.log(obj.firstName);
+    console.log(obj.gender);  
+}
+
+const ans = printDetails(person)
+console.log(ans);
+
