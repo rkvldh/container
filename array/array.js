e1cfa0cf4d774b9e7e10917e88b082921acf4d3b
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2024-10-17 04:28:04 +0000

    array practice

---
+++ array/array.js
@@ -0,0 +1,18 @@
+// let marvelHeros = ['thor', 'spiderman', 'ironman', 'antman', 'Dr. Strange']
+// let indianHeros = ['shaktiman', 'krissh'];
+// let heros = marvelHeros .concat(indianHeros);
+// let newHeros = marvelHeros.concat(marvelHeros);
+
+// console.log(allHeros.toString());
+// console.log(allHeros.slice(1,4));
+// let Heros = marvelHeros.splice(3,0,'DaraSingh');
+// console.log(newHeros);
+// console.log('conveert to string ', marvelHeros.toString());
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// arr.splice(2, 0, 'dara', 'singh');
+// console.log(arr);
+
+
+
+
---
+++ practice.js
@@ -0,0 +1,15 @@
+let companies = ['Bllomgerg', 'Microshoft', 'Uber', 'Google', 'Netflix'];
+console.log(companies);
+// remoove first array
+companies.shift()
+console.log(companies);
+// add ola in 3rd index
+companies.splice(2,0, 'Ola')
+console.log(companies);
+// add amozon in last index
+companies.push('Amazon')
+console.log(companies);
+
+
+
+
