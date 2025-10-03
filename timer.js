d1116af634456765c4368cde71ed191bfa0bdbb1
Ramesh Viahwakarma <74896769+rkvldh@users.noreply.github.com>
2025-10-03 04:34:06 +0000

    js timers

---
+++ timer.js
@@ -0,0 +1,11 @@
+
+
+// setTimeout, clearTimeout
+// setTimeout(function(){   
+//     console.log('hello world');
+// },5000)
+// setInterval, clearInterval repete time duration bar bar chalta hai
+let tm = setInterval(function(){
+    console.log(tm);
+},2000)
+// clearInterval(tm);
